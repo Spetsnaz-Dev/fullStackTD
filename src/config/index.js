24b83a85b88f6bcf1286b827990ae5c522858b49
Spetsnaz-Dev <ravindrafk@gmail.com>
2024-10-11 14:45:23 +0530

    first commit

---
+++ src/config/index.js
@@ -0,0 +1,23 @@
+export const signupFormControls = [
+  {
+    name: "username",
+    label: "User Name",
+    placeholder: "Enter your user name here",
+    type: "text",
+    componentType: "input",
+  },
+  {
+    name: "email",
+    label: "User Email",
+    placeholder: "Enter your user email here",
+    type: "email",
+    componentType: "input",
+  },
+  {
+    name: "password",
+    label: "Password",
+    placeholder: "Enter your user password here",
+    type: "password",
+    componentType: "input",
+  },
+];
---
+++ src/pages/auth/index.jsx
@@ -0,0 +1,43 @@
+import CommonForm from "@/components/common-form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { signupFormControls } from "@/config";
+import { GraduationCap } from "lucide-react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function AuthPage() {
+  const [activeTab, setActiveTab] = useState("signin");
+
+  function handleTabChange(value) {
+    setActiveTab(value);
+  }
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="px-4 lg:px-6 h-14 flex items-center border-botton">
+        <Link to={"/"} className="flex items-center justify-center">
+          <GraduationCap className="h-8 w-8 mr-4" />
+          <span className="font-extrabold text text-xl">LMS Learn</span>
+        </Link>
+      </header>
+      <div className="flex items-center justify-center min-h-screen bg-background">
+        <Tabs
+          value={activeTab}
+          defaultValue="signin"
+          onValueChange={handleTabChange}
+          className="w-full max-w-md"
+        >
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="signin">Sign In</TabsTrigger>
+            <TabsTrigger value="signup">Sign Up</TabsTrigger>
+          </TabsList>
+          <TabsContent value="signin">Signin</TabsContent>
+          <TabsContent value="signup">
+            <CommonForm formControls={signupFormControls} />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
+}
+
+export default AuthPage;
